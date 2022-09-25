e01acd24ac6d62c655b065dc7bf941d9f72f50b7
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2022-09-25 16:03:46 +0200

    Add files via upload

---
+++ lamp.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("btn");
+let light = document.getElementById("light");
+
+
+
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
